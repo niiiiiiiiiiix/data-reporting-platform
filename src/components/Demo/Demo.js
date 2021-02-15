21b1b1d045e43244918b6d53ebb536bafa2dafe6
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-02-15 20:02:26 +0800

    Add mock data file

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -1,9 +1,13 @@
 import React from "react";
+import randomData from "./data";
 
 const Demo = () => {
   return (
     <div>
-      <h1>This is your demo page</h1>;
+      <h1>This is your demo page</h1>
+      <div>
+        <randomData />
+      </div>
     </div>
   );
 };
--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1>This is your Home Page</h1>;
+      <h1>This is your Home Page</h1>
     </div>
   );
 };
