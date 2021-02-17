abfe6cd8044d8ab0b73a9c857c4328977f831b24
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-02-17 16:26:53 +0800

    Use only draggable

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -5,60 +5,39 @@ import ShirtSizeBarChart from "./ShirtSizeBarChart";
 import interact from "interactjs";
 import "./Demo.css";
 
-interact(".test")
-  .resizable({
-    // resize from all edges and corners
-    edges: { left: true, right: true, bottom: true, top: true },
+// target elements with the "draggable" class
+interact(".test").draggable({
+  //enable inertial throwing
+  inertia: true,
+  // keep the element within the area of it's parent
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restrictions: "parent",
+      endOnly: true,
+    }),
+  ],
+  // enable autoScroll
+  autoScroll: true,
+  listeners: {
+    // call this function on every dragmove event
+    move: dragMoveListener,
+  },
+});
 
-    listeners: {
-      move(event) {
-        let target = event.target;
-        let x = parseFloat(target.getAttribute("data-x")) || 0;
-        let y = parseFloat(target.getAttribute("data-y")) || 0;
+function dragMoveListener(event) {
+  let target = event.target;
+  // keep the dragged position in the data-x/data-y attributes
+  let x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
 
-        // update the element's style
-        target.style.width = event.rect.width + "px";
-        target.style.height = event.rect.height + "px";
+  // translate the element
+  target.style.webkitTransform = target.style.transform =
+    "translate(" + x + "px, " + y + "px)";
 
-        // translate when resizing from top or left edges
-        x += event.deltaRect.left;
-        y += event.deltaRect.top;
-
-        target.style.webkitTransform = target.style.transform =
-          "translate(" + x + "px," + y + "px)";
-
-        target.setAttribute("data-x", x);
-        target.setAttribute("data-y", y);
-        // target.textContent =
-        //   Math.round(event.rect.width) +
-        //   "\u00D7" +
-        //   Math.round(event.rect.height);
-      },
-    },
-    modifiers: [
-      // keep the edges inside the parent
-      interact.modifiers.restrictEdges({
-        outer: "parent",
-      }),
-
-      // minimum size
-      interact.modifiers.restrictSize({
-        min: { width: 100, height: 50 },
-      }),
-    ],
-
-    inertia: true,
-  })
-  .draggable({
-    listeners: { move: window.dragMoveListener },
-    inertia: true,
-    modifiers: [
-      interact.modifiers.restrictRect({
-        restriction: "parent",
-        endOnly: true,
-      }),
-    ],
-  });
+  //update the position attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
 
 function Demo() {
   return (
