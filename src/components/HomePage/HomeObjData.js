c010820a594c9f7b8f731260e64e852d637ffffb
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-02-19 12:34:53 +0800

    CSS styling

--- src/components/HomePage/HomeObjData.js
+++ src/components/HomePage/HomeObjData.js
@@ -21,8 +21,8 @@ export const HomeObjTwo = {
   description:
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
   buttonLabel: "Sample button",
-  imgStart: "",
-  img: "images/svg-2.svg",
+  imgStart: "start",
+  img: "images/svg-1.svg",
   alt: "",
 };
 
