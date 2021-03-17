9eb569fc948f6b89f3f08b44eccca07178ebbc34
Nix Tay <niiiiiiiiiiix@users.noreply.github.com>
2021-03-17 10:49:28 +0800

    Remove old code from Demo.js

--- src/components/Demo/Demo.js
+++ src/components/Demo/Demo.js
@@ -32,7 +32,7 @@ const Demo = () => {
             </li>
           ))}
           <div>
-            <table className="table-content">
+            <table className="table-content" id="table-content">
               <thead>
                 <tr>
                   <th>Product Name</th>
@@ -43,71 +43,21 @@ const Demo = () => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>
-                    <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-                      The One Scoop Tee
-                    </a>
-                  </td>
-                  <td>$19.99</td>
-                  <td>$10.00</td>
-                  <td>16 Mar 2021, 12:28:55</td>
-                  <td>
-                    <ImCross />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-                      The One Scoop Tee
-                    </a>
-                  </td>
-                  <td>$19.99</td>
-                  <td>$10.00</td>
-                  <td>16 Mar 2021, 12:28:55</td>
-                  <td>
-                    <ImCross />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-                      The One Scoop Tee
-                    </a>
-                  </td>
-                  <td>$19.99</td>
-                  <td>$10.00</td>
-                  <td>16 Mar 2021, 12:28:55</td>
-                  <td>
-                    <ImCross />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-                      The One Scoop Tee
-                    </a>
-                  </td>
-                  <td>$19.99</td>
-                  <td>$10.00</td>
-                  <td>16 Mar 2021, 12:28:55</td>
-                  <td>
-                    <ImCross />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-                      The One Scoop Tee
-                    </a>
-                  </td>
-                  <td>$19.99</td>
-                  <td>$10.00</td>
-                  <td>16 Mar 2021, 12:28:55</td>
-                  <td>
-                    <ImCross />
-                  </td>
-                </tr>
+                {wishlist.map((item) => {
+                  return (
+                    <tr>
+                      <td>
+                        <a href={item.productLink}>{item.productName}</a>
+                      </td>
+                      <td>{item.originalPrice}</td>
+                      <td>{item.salesPrice}</td>
+                      <td>{item.lastUpdated}</td>
+                      <td>
+                        <ImCross />
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
@@ -116,99 +66,5 @@ const Demo = () => {
     </div>
   );
 };
-
-// function Demo() {
-//   return (
-//     <div className="bodyy">
-//       <div className="demo-container">
-//         <h1>This is your demo page</h1>
-//         <div className="action-bar">Action Bar for sort/filter/refresh</div>
-//         <div className="wishlist">
-//           Actual list of items
-//           <div>
-//             <table className="table-content">
-//               <thead>
-//                 <tr>
-//                   <th>Product Name</th>
-//                   <th>Original Price</th>
-//                   <th>Sales Price</th>
-//                   <th>Last Updated</th>
-//                   <th>Delete</th>
-//                 </tr>
-//               </thead>
-//               <tbody>
-//                 <tr>
-//                   <td>
-//                     <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-//                       The One Scoop Tee
-//                     </a>
-//                   </td>
-//                   <td>$19.99</td>
-//                   <td>$10.00</td>
-//                   <td>16 Mar 2021, 12:28:55</td>
-//                   <td>
-//                     <ImCross />
-//                   </td>
-//                 </tr>
-//                 <tr>
-//                   <td>
-//                     <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-//                       The One Scoop Tee
-//                     </a>
-//                   </td>
-//                   <td>$19.99</td>
-//                   <td>$10.00</td>
-//                   <td>16 Mar 2021, 12:28:55</td>
-//                   <td>
-//                     <ImCross />
-//                   </td>
-//                 </tr>
-//                 <tr>
-//                   <td>
-//                     <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-//                       The One Scoop Tee
-//                     </a>
-//                   </td>
-//                   <td>$19.99</td>
-//                   <td>$10.00</td>
-//                   <td>16 Mar 2021, 12:28:55</td>
-//                   <td>
-//                     <ImCross />
-//                   </td>
-//                 </tr>
-//                 <tr>
-//                   <td>
-//                     <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-//                       The One Scoop Tee
-//                     </a>
-//                   </td>
-//                   <td>$19.99</td>
-//                   <td>$10.00</td>
-//                   <td>16 Mar 2021, 12:28:55</td>
-//                   <td>
-//                     <ImCross />
-//                   </td>
-//                 </tr>
-//                 <tr>
-//                   <td>
-//                     <a href="https://cottonon.com/SG/the-one-scoop-tee/2009222-02.html?dwvar_2009222-02_color=2009222-02&cgid=sale-womens&originalPid=2009222-02#start=20">
-//                       The One Scoop Tee
-//                     </a>
-//                   </td>
-//                   <td>$19.99</td>
-//                   <td>$10.00</td>
-//                   <td>16 Mar 2021, 12:28:55</td>
-//                   <td>
-//                     <ImCross />
-//                   </td>
-//                 </tr>
-//               </tbody>
-//             </table>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 export default Demo;
